561c344b1c79ce5539a3d587d68006d3adcb7a3c
soso <wowsososo@gmail.com>
2012-05-28 20:41:09 +0800

    quick call for conns fuck

--- conn.c
+++ conn.c
@@ -13,15 +13,15 @@ void conns_extend(conns_t *conns, int count)
     int conn_info_t_size = sizeof(conn_info_t);
 
     void *p = realloc(
-        conns.conn_info, 
-        conns.total * conn_info_t_size + count * conn_info_t_size);
+        conns->conn_info, 
+        conns->total * conn_info_t_size + count * conn_info_t_size);
 
     if(p == NULL) {
         printf("realloc error");
         exit(-1);
     }
 
-    conns.total += count;
+    conns->total += count;
 }
 
 void conns_cut(conns_t conns)
@@ -32,19 +32,19 @@ void conns_cut(conns_t conns)
 void conn_add(conns_t *conns, int fd, int slave_id)
 {
     
-    if(conns.count < conns.total) {
-        conns.conn_info[conns.count].fd = fd;
-        conns.conn_info[conns.count].slave_id = slave_id;
-        conns.count++;
+    if(conns->count < conns.total) {
+        conns->conn_info[conns.count].fd = fd;
+        conns->conn_info[conns.count].slave_id = slave_id;
+        conns->count++;
         return NULL;
     }
     
-    else if(conns.total == 0) {
-        conns.conn_info = malloc(INIT_CONN_COUNT * sizeof(conn_info_t));
-        conns.conn_info[0].fd = fd;
-        conns.conn_info[0].slave_id = slave_id;
-        conns.count = 1;
-        conns.total = INIT_CONN_COUNT;
+    else if(conns->total == 0) {
+        conns->conn_info = malloc(INIT_CONN_COUNT * sizeof(conn_info_t));
+        conns->conn_info[0].fd = fd;
+        conns->conn_info[0].slave_id = slave_id;
+        conns->count = 1;
+        conns->total = INIT_CONN_COUNT;
         return NULL;
     }
     
